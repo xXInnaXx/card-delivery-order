89eea61fc17818589096d2b767f9adb0841ae2d2
innakhabirova <iikhabi@mail.ru>
2022-04-14 23:21:47 +0300

    mistakes were fixed

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -19,49 +19,54 @@ import static com.codeborne.selenide.Selenide.*;
 import static java.time.LocalDate.now;
 
 public class DeliveryCardTest {
-    private LocalDate nowPlusThreeDays= now().plusDays(3);
-    DateTimeFormatter df = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    @BeforeAll
-    public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+    }
+
 
     @Test
     public void shouldSendForm() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(4);
         $("[placeholder='Город']").setValue("Казань");
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79111234455");
-        $("[data-test-id=date] input").setValue(df.format(nowPlusThreeDays));
+        $("[data-test-id=date] input").setValue(planningDate);
         $((".checkbox__text")).click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byXpath("//div[@class='notification__content']")).shouldBe(visible);
-
+        $(byXpath("//div[@class='notification__content']")).shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 
     @Test
     public void shouldNotSendFormWithIncorrectCity() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
         $("[placeholder='Город']").setValue("ghggj");
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79111234455");
-        $("[data-test-id=date] input").setValue(df.format(nowPlusThreeDays));
+        $("[data-test-id=date] input").setValue(planningDate);
         $((".checkbox__text")).click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Доставка в выбранный город недоступна")).shouldBe(visible, Duration.ofSeconds(15));
         $(byXpath("//div[@class='notification__content']")).shouldBe(hidden);
 
     }
+
     @Test
     public void shouldNotSendFormWithIncorrectDate() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(-3);
         $("[placeholder='Город']").setValue("Тверь");
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79111234455");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(df.format(now().minusDays(3)));
+        $("[data-test-id=date] input").setValue(planningDate);
         $((".checkbox__text")).click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Заказ на выбранную дату невозможен")).shouldBe(visible, Duration.ofSeconds(15));
@@ -71,11 +76,11 @@ public class DeliveryCardTest {
 
     @Test
     public void shouldNotSendNoRU() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
         $("[placeholder='Город']").setValue("Тверь");
         $("[name='name']").setValue("Ivanov Ivan");
         $("[name='phone']").setValue("+79111234455");
-        $("[data-test-id=date] input").setValue(df.format(nowPlusThreeDays));
+        $("[data-test-id=date] input").setValue(planningDate);
         $((".checkbox__text")).click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).shouldBe(visible, Duration.ofSeconds(15));
@@ -85,41 +90,42 @@ public class DeliveryCardTest {
 
     @Test
     public void shouldNotSendWithNumberPhoneMoreThan11() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
         $("[placeholder='Город']").setValue("Тверь");
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79111234455798789");
-        $("[data-test-id=date] input").setValue(df.format(nowPlusThreeDays));
+        $("[data-test-id=date] input").setValue(planningDate);
         $((".checkbox__text")).click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(visible, Duration.ofSeconds(15));
         $(byXpath("//div[@class='notification__content']")).shouldBe(hidden);
 
     }
+
     @Test
     public void shouldNotSendWithNumberPhoneWithoutPlus() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
         $("[placeholder='Город']").setValue("Тверь");
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("79111234455");
-        $("[data-test-id=date] input").setValue(df.format(nowPlusThreeDays));
+        $("[data-test-id=date] input").setValue(planningDate);
         $((".checkbox__text")).click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(visible, Duration.ofSeconds(15));
         $(byXpath("//div[@class='notification__content']")).shouldBe(hidden);
 
     }
+
     @Test
     public void shouldNotSendIfCheckboxNotSelected() throws InterruptedException {
-        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
         $("[placeholder='Город']").setValue("Тверь");
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79111234455");
-        $("[data-test-id=date] input").setValue(df.format(nowPlusThreeDays));
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=agreement] input");
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Я соглашаюсь с условиями обработки и использования моих персональных данных")).shouldBe(visible, Duration.ofSeconds(15));
         $(byXpath("//div[@class='notification__content']")).shouldBe(hidden);
-
     }
 }
